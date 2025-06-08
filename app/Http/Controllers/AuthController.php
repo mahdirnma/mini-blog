1a69ca27f764638ce55002f28b386092f90436c1
mahdirnma <rahnamamahdi78@gmail.com>
2025-06-08 19:22:20 +0330

    create login and register forms

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function loginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function registerForm()
+    {
+        return view('auth.register');
+    }
 }
