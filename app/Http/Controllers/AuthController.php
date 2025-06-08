fcc169acd501afd065c25ed0f556e423cb2a3c38
mahdirnma <rahnamamahdi78@gmail.com>
2025-06-08 23:30:51 +0330

    create login process

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserLoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -11,6 +13,14 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
+    public function login(StoreUserLoginRequest $request)
+    {
+        $myData = $request->only(['email', 'password']);
+        if (Auth::attempt($myData)) {
+            return to_route('dashboard');
+        }
+        return to_route('login.form');
+    }
     public function registerForm()
     {
         return view('auth.register');
